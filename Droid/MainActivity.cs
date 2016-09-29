7ee438a234bed2ff46eab33a3091463b246b738f
Rui Marinho <me@ruimarinho.net>
2016-09-29 21:53:40 +0100

    [Android] Add android basic implementation

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -7,22 +7,44 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Xamarin.Forms;
+using System.Globalization;
+using Xamarin.Forms.Platform.Android;
 
 namespace nativemix.Droid
 {
-    [Activity(Label = "nativemix.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
-    {
-        protected override void OnCreate(Bundle bundle)
-        {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
+	[Activity(Label = "nativemix.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			TabLayoutResource = Resource.Layout.Tabbar;
+			ToolbarResource = Resource.Layout.Toolbar;
 
-            base.OnCreate(bundle);
+			base.OnCreate(bundle);
 
-            global::Xamarin.Forms.Forms.Init(this, bundle);
+			global::Xamarin.Forms.Forms.Init(this, bundle);
+			var s = Forms.Context;
+			LoadApplication(new App());
+		}
+	}
 
-            LoadApplication(new App());
-        }
-    }
+	public class DroidColorConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is Color)
+				return ((Color)value).ToAndroid();
+
+			return null;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is global::Android.Graphics.Color)
+				return ((global::Android.Graphics.Color)value).ToColor();
+
+			return null;
+		}
+	}
 }
